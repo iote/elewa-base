c4a205dfcf6669b741e51b383e9993dd329de2cf
Jente Rosseel <rosseeljente@gmail.com>
2018-02-20 16:25:02 +0100

    Remove redundant imports

--- elewa-backend/src/app.module.ts
+++ elewa-backend/src/app.module.ts
@@ -1,17 +1,17 @@
-import { Module, MiddlewaresConsumer, NestModule, RequestMethod,} from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { connString } from './base/config/db.connectionstring';
+import { Module, NestModule, MiddlewaresConsumer, RequestMethod } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { GraphQLModule, GraphQLFactory } from "@nestjs/graphql";
 
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
-import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
 
-import { ServerBootModule } from './modules/_server-boot/server-boot.module';
-import { CurriculumModule } from './modules/curriculum/curriculum.module';
+import { ServerBootModule } from "./modules/_server-boot/server-boot.module";
+import { AuthModule } from "./modules/auth/auth.module";
+import { CurriculumModule } from "./modules/curriculum/curriculum.module";
 
-import { AppController } from './app.controller';
-import { AuthModule } from './modules/auth/auth.module';
-import { production } from './base/config/production';
-import * as passport from 'passport';
+import { AppController } from "./app.controller";
+
+import { production } from "./base/config/production";
+import { connString } from "./base/config/db.connectionstring";
 
 @Module({
   imports: [ MongooseModule.forRoot(connString),
